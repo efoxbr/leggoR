b86eb36496dbc966ce1cb16d9e2db98ce8f6e663
Tarciso Oliveira <tarcisocomp@gmail.com>
2018-10-22 17:47:41 -0300

    Recupera lista de eventos para as duas casas.

---
+++ scripts/get_eventos_congresso.R
@@ -0,0 +1,9 @@
+url_eventos_camara <- "https://dadosabertos.camara.leg.br/api/v2/referencias/tiposTramitacao"
+eventos_camara <- jsonlite::fromJSON(url_eventos_camara, flatten = T)$dados %>%
+  dplyr::mutate(id = as.integer(id)) %>%
+  dplyr::arrange(id)
+
+url_eventos_senado <- "http://legis.senado.gov.br/dadosabertos/materia/situacoes"
+eventos_senado <- jsonlite::fromJSON(url_eventos_senado, flatten = T)$ListaSituacoes$Situacoes$Situacao %>%
+  dplyr::mutate(Codigo = as.integer(Codigo)) %>%
+  dplyr::arrange(Codigo)
